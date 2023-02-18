bc10002c77ef0316d969a810c7f5a1c769366295
C-SR <125709440+C-SR@users.noreply.github.com>
2023-02-18 15:34:41 -0500

    Update Game_pt_cross.js

--- src/pages/games/Game_pt_cross.js
+++ src/pages/games/Game_pt_cross.js
@@ -5,14 +5,14 @@ import Crossword from '@jaredreisinger/react-crossword';
 const data = {
     down: {
         1: {
-          clue: 'answer for 1',
-          answer: 'answer1',
+          clue: 'Biggest start in universe',
+          answer: 'SOL',
           row: 0,
           col: 4,
         },
         2: {
-            clue: 'answer for 2',
-            answer: 'answer2',
+            clue: 'Basic element for humans',
+            answer: 'AR',
             row: 2,
             col: 3,
           },
@@ -20,20 +20,20 @@ const data = {
       },
       across: {
         3: {
-          clue: 'answer for 3',
-          answer: 'answer3',
+          clue: 'Synonym for faithful',
+          answer: 'LEAL',
           row: 2,
           col: 1,
         },
         4: {
-            clue: 'answer for 4',
-            answer: 'answer4',
+            clue: 'When people strongly believe on an ideology',
+            answer: 'FE',
             row: 3,
             col: 0,
           },
           5: {
-            clue: 'answer for 5',
-            answer: 'answer5',
+            clue: 'Husband of the queen',
+            answer: 'REI',
             row: 4,
             col: 1,
           },
